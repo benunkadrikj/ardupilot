7f4e2eee89e30138da1626e4c07465bf92f36c44
benunkadrikj <benunk@t.mk>
2021-09-08 20:13:11 +0200

    Update AP_GPS_UBLOX.cpp

--- libraries/AP_GPS/AP_GPS_UBLOX.cpp
+++ libraries/AP_GPS/AP_GPS_UBLOX.cpp
@@ -145,7 +145,7 @@ const AP_GPS_UBLOX::config_list AP_GPS_UBLOX::config_MB_Base_uart1[] {
 
 const AP_GPS_UBLOX::config_list AP_GPS_UBLOX::config_MB_Base_uart2[] {
  { ConfigKey::CFG_UART2_ENABLED, 1},
- { ConfigKey::CFG_UART2_BAUDRATE, 460800},
+ { ConfigKey::CFG_UART2_BAUDRATE, 115200},
  { ConfigKey::CFG_UART2OUTPROT_RTCM3X, 1},
  { ConfigKey::CFG_UART1OUTPROT_RTCM3X, 0},
  { ConfigKey::CFG_UART1INPROT_RTCM3X, 1},
@@ -198,7 +198,7 @@ const AP_GPS_UBLOX::config_list AP_GPS_UBLOX::config_MB_Rover_uart1[] {
 
 const AP_GPS_UBLOX::config_list AP_GPS_UBLOX::config_MB_Rover_uart2[] {
  { ConfigKey::CFG_UART2_ENABLED, 1},
- { ConfigKey::CFG_UART2_BAUDRATE, 460800},
+ { ConfigKey::CFG_UART2_BAUDRATE, 115200},
  { ConfigKey::CFG_UART2OUTPROT_RTCM3X, 0},
  { ConfigKey::CFG_UART2INPROT_RTCM3X, 1},
  { ConfigKey::CFG_UART1INPROT_RTCM3X, 0},
@@ -939,7 +939,7 @@ AP_GPS_UBLOX::_parse_gps(void)
     if (_class == CLASS_CFG) {
         switch(_msg_id) {
         case  MSG_CFG_NAV_SETTINGS:
-	    Debug("Got settings %u min_elev %d drLimit %u\n", 
+	    Debug("Got settings %u min_elev %d drLimit %u\n",
                   (unsigned)_buffer.nav_settings.dynModel,
                   (int)_buffer.nav_settings.minElev,
                   (unsigned)_buffer.nav_settings.drLimit);
@@ -1033,7 +1033,7 @@ AP_GPS_UBLOX::_parse_gps(void)
 
         case MSG_CFG_SBAS:
             if (gps._sbas_mode != AP_GPS::SBAS_Mode::DoNotChange) {
-	        Debug("Got SBAS settings %u %u %u 0x%x 0x%x\n", 
+	        Debug("Got SBAS settings %u %u %u 0x%x 0x%x\n",
                       (unsigned)_buffer.sbas.mode,
                       (unsigned)_buffer.sbas.usage,
                       (unsigned)_buffer.sbas.maxSBAS,
@@ -1082,11 +1082,11 @@ AP_GPS_UBLOX::_parse_gps(void)
                 _unconfigured_messages &= ~CONFIG_RATE_NAV;
             }
             return false;
-            
+
 #if CONFIGURE_PPS_PIN
         case MSG_CFG_TP5: {
             // configure the PPS pin for 1Hz, zero delay
-            Debug("Got TP5 ver=%u 0x%04x %u\n", 
+            Debug("Got TP5 ver=%u 0x%04x %u\n",
                   (unsigned)_buffer.nav_tp5.version,
                   (unsigned)_buffer.nav_tp5.flags,
                   (unsigned)_buffer.nav_tp5.freqPeriod);
@@ -1183,14 +1183,14 @@ AP_GPS_UBLOX::_parse_gps(void)
             break;
         case MSG_MON_HW2:
             if (_payload_length == 28) {
-                log_mon_hw2();  
+                log_mon_hw2();
             }
             break;
         case MSG_MON_VER:
             _have_version = true;
             strncpy(_version.hwVersion, _buffer.mon_ver.hwVersion, sizeof(_version.hwVersion));
             strncpy(_version.swVersion, _buffer.mon_ver.swVersion, sizeof(_version.swVersion));
-            GCS_SEND_TEXT(MAV_SEVERITY_INFO, 
+            GCS_SEND_TEXT(MAV_SEVERITY_INFO,
                                              "u-blox %d HW: %s SW: %s",
                                              state.instance + 1,
                                              _version.hwVersion,
@@ -1400,7 +1400,7 @@ AP_GPS_UBLOX::_parse_gps(void)
         state.location.lng    = _buffer.pvt.lon;
         state.location.lat    = _buffer.pvt.lat;
         state.location.alt    = _buffer.pvt.h_msl / 10;
-        switch (_buffer.pvt.fix_type) 
+        switch (_buffer.pvt.fix_type)
         {
             case 0:
                 state.status = AP_GPS::NO_FIX;
@@ -1440,7 +1440,7 @@ AP_GPS_UBLOX::_parse_gps(void)
         state.have_vertical_accuracy = true;
         // SVs
         state.num_sats    = _buffer.pvt.num_sv;
-        // velocity     
+        // velocity
         _last_vel_time         = _buffer.pvt.itow;
         state.ground_speed     = _buffer.pvt.gspeed*0.001f;          // m/s
         state.ground_course    = wrap_360(_buffer.pvt.head_mot * 1.0e-5f);       // Heading 2D deg * 100000
@@ -1456,9 +1456,9 @@ AP_GPS_UBLOX::_parse_gps(void)
             state.hdop        = _buffer.pvt.p_dop;
             state.vdop        = _buffer.pvt.p_dop;
         }
-                    
+
         state.last_gps_time_ms = AP_HAL::millis();
-        
+
         // time
         state.time_week_ms    = _buffer.pvt.itow;
 #if UBLOX_FAKE_3DLOCK
